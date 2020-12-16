49eadf33c1ba349915ab440a8605073aa79a97db
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-12-16 13:57:01 -0800

    added followers column

---
+++ src/components/FollowersColumn.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FollowersColumn = () =>
+{
+    return (
+        <div className='followers-column'>
+            <h1>Followers</h1>
+            <div className='home'>
+
+            </div>
+        </div>
+    )
+}
+
+export default FollowersColumn
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Upload from './pages/Upload';
 import Header from './components/Header';
-import './index.css';
+import './App.css';
 
 const App = () => {
   return (
