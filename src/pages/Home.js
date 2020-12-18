0052ea904a432bb62a7252638d97b153a67c8430
IrinaAlcala <58370677+IrinaAlcala@users.noreply.github.com>
2020-12-18 14:46:42 -0800

    added carrd and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import FollowersColumn from '../components/FollowersColumn'
+import Card from '../components/Card'
+import axios from 'axios'
 
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
             <FollowersColumn />
+            <Card />
+            
         </div>
     );
 }
